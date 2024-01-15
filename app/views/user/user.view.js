0aa7d0dd1e253d5e5f616d7d287c33b782e1fd89
Naufal Mahadian <naupl.ramadhan@gmail.com>
2024-01-15 09:32:29 +0700

    cleaning code

--- app/views/user/user.view.js
+++ app/views/user/user.view.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     // DOM elements
-    const userListContainer = document.getElementById('app');
+    const userListContainer = document.getElementById('app')
 
     // Fetch all users
     const fetchAllUsers = async () => {
         try {
-            const response = await fetch('http://localhost:3000/api/user');
-            const users = await response.json();
+            const response = await fetch('http://localhost:3000/api/user')
+            const users = await response.json()
             
-            // Render user list
-            renderUserList(users);
+            // users list
+            renderUserList(users)
         } catch (error) {
-            console.error('Error fetching users:', error);
+            console.error('Error fetching users:', error)
         }
-    };
+    }
 
     // Render user list
     const renderUserList = (users) => {
-        userListContainer.innerHTML = '';
+        userListContainer.innerHTML = ''
 
         if (users.length === 0) {
-            userListContainer.innerHTML = '<p>No users found.</p>';
-            return;
+            userListContainer.innerHTML = '<p>No users found.</p>'
+            return
         }
 
-        const userList = document.createElement('ul');
+        const userList = document.createElement('ul')
         users.forEach((user) => {
-            const listItem = document.createElement('li');
-            listItem.textContent = user.email; 
-            userList.appendChild(listItem);
+            const listItem = document.createElement('li')
+            listItem.textContent = user.email
+            userList.appendChild(listItem)
         });
 
-        userListContainer.appendChild(userList);
-    };
+        userListContainer.appendChild(userList)
+    }
 
     // Initial fetch
     fetchAllUsers();
-});
+})
